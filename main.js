514048bc749aa422788fd1c479b7802604d0e917
Collin Porter <collinporter7@gmail.com>
2022-07-17 19:18:03 -0500

    Working PainJS

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function onMouseMove(event) {
-  tool.fixedDistance = Math.floor(Math.floor(Math.random() * 2) +1)
+  tool.fixedDistance = Math.floor(Math.floor(Math.random() * 2) + 1)
   var path = new Path.Circle({
     center: event.middlePoint,
     radius: Math.round(Math.random() * 25) + 5
